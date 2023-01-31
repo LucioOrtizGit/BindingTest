e6560b2a5f02d7db3bef443e09b35e7273057e35
LucioOrtizGit <lukefilms@gmail.com>
2023-01-31 12:16:05 +0100

    Agregar archivos de proyecto.

---
+++ BindingTest/MainActivity.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.Widget;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+using Com.Xamarin.Textcounter;
+using Android.Views.InputMethods;
+using Android.Widget;
+using Android.Content;
+
+namespace BindingTest
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            // Utilizacion de la libreria AarBinding
+            var imm = (InputMethodManager)GetSystemService(Context.InputMethodService);
+
+            var vowelsBtn = FindViewById<Button>(Resource.Id.vowels);
+            var consonBtn = FindViewById<Button>(Resource.Id.consonants);
+            var edittext = FindViewById<EditText>(Resource.Id.input);
+            edittext.InputType = Android.Text.InputTypes.TextVariationPassword;
+
+            edittext.KeyPress += (sender, e) =>
+            {
+                imm.HideSoftInputFromWindow(edittext.WindowToken, HideSoftInputFlags.NotAlways);
+                e.Handled = true;
+            };
+
+            vowelsBtn.Click += (sender, e) =>
+            {
+                int count = TextCounter.NumVowels(edittext.Text);
+                string msg = count + " vowels found.";
+                Toast.MakeText(this, msg, ToastLength.Short).Show();
+            };
+
+            consonBtn.Click += (sender, e) =>
+            {
+                int count = TextCounter.NumConsonants(edittext.Text);
+                string msg = count + " consonants found.";
+                Toast.MakeText(this, msg, ToastLength.Short).Show();
+            };
+
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (View.IOnClickListener)null).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+}
